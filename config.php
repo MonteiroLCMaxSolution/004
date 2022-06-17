8db7b88f4cb3b16ffba044564779424d795df7e2
Monteiro <foxs.sbc11@gmail.com>
2022-06-17 12:41:54 -0300

    Arquivo Principal

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 	
+	$dominio = $_SERVER['HTTP_HOST'];
+	//$dominio = 'lcmaxsolution.com.br';
+		$ambiente = "";
+		$http = "https://".$dominio."/".$ambiente;
+		$NomeEmpresa = '3E Comercial';
+		$LogoEmpresa = 'logo.png';
+		$LogoEmpresaBranco = 'logo_branco.png';
+		$IconEmpresa = 'icon.jpg';
+		$tituloEmpresa = "3E Comercial - Distribuidora de materiais para construção"
+				
+			
+		?>
